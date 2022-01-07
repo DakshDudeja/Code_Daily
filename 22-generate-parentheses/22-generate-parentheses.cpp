4b3b8036e1cc847686b38cfaa840248d544084a6
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-01-07 11:20:48 +0530

    Time: 16 ms (9.66%), Space: 13.8 MB (46.52%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,23 @@
+
+  class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> output_arr;
+        backtracking(output_arr, "", 0, 0, n);
+        return output_arr;
+    }
+public:
+    void backtracking(vector<string> &output_arr , string current_string, int open, int close, int &max){
+        if(current_string.length()==max*2){
+            output_arr.push_back(current_string);
+            return;
+        }
+        
+        if(open < max){
+            backtracking(output_arr, current_string + "(", open + 1, close, max);
+        }
+        if(close < open){
+            backtracking(output_arr, current_string + ")", open, close + 1, max);
+        }
+    }
+};
