b0d13868004af637d992d89668eaf6ba4fa5e445
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-01-22 14:06:02 +0530

    Time: 87 ms (49.33%), Space: 12 MB (37.25%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int dp[100001];
     bool solve(int n){
-        if(n <= 0) return false;
+        if(n < 0) return 0;
         else if(dp[n] != -1) return dp[n];
         
         for(int i = 1; i*i <= n;i++){
