fa2d63d901d0868aa04388b361158201909b5e2b
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-01-22 14:01:28 +0530

    Time: 44 ms (67.45%), Space: 12.2 MB (26.17%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-        int dp[100001];
-
+    int dp[100001];
     bool solve(int n){
-       if(n==0) return false; 
-        if(dp[n]!=-1) return dp[n];
-        bool ans = false;
+        if(n <= 0) return false;
+        else if(dp[n] != -1) return dp[n];
         
-        for(int i=1;i*i<=n;i++){
-           ans=ans || !solve(n-i*i);
+        for(int i = 1; i*i <= n;i++){
+            int sq = i*i;
+            if(solve(n-sq) == 0) return dp[n] = 1;
         }
-        return dp[n]=ans;
+        
+        return dp[n] = 0;
     }
+    
     bool winnerSquareGame(int n) {
-         memset(dp,-1,sizeof(dp));
+        memset(dp,-1,sizeof dp);
         return solve(n);
     }
 };
