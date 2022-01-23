d3fcc45aa28c52f67f977ebcb4dcf6a44eed23ee
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-01-23 13:09:41 +0530

    Time: 12 ms (50.67%), Space: 16.4 MB (25.03%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    // bool solve(vector<int>& piles,int idx,bool alice,int sumA,int sumB){
-    //     if(idx>piles.size()) return false;
-    //     if(idx==piles.size()){
-    //         if(sumA>sumB) return true;
-    //         else
-    //             return false;
-    //     }
-    //     for(int i=0;i<piles.size();i++){
-    //         if(alice==true)
-    //             solve(piles,idx+1,false,sumA+max(piles[i],piles[piles.size()-1]),sumB);
-    //         else
-    //             solve(piles,idx+1,true,sumA,sumB+max(piles[i],piles[piles.size()-1]));
-    //     }
-    // }
-    bool stoneGame(vector<int>& piles) {
-        // solve(piles,true,0,0)
-        return 1;
+   vector<vector<int>>dp;
+bool checkIfAliceWin(int turn,vector<int>& piles,int ali,int bob,int start,int end){
+    
+    if(start == end){
+        if(turn == 0) ali += piles[start];
+        else bob += piles[end];
+        
+        return ali > bob;
     }
+    
+    if(dp[start][end] != -1) return dp[start][end];
+    
+    if(turn == 0) dp[start][end] = checkIfAliceWin(1,piles,ali+piles[start],bob,start+1,end) || checkIfAliceWin(1,piles,ali+piles[end],bob,start,end-1);
+    else dp[start][end] = checkIfAliceWin(0,piles,ali,bob+piles[start],start+1,end) || checkIfAliceWin(0,piles,ali,bob+piles[end],start,end-1);
+    
+    return dp[start][end];
+}
+
+bool stoneGame(vector<int>& piles) {
+    
+  dp.resize(piles.size(), vector<int>(piles.size(), -1));
+  
+  return checkIfAliceWin(0,piles,0,0,0,piles.size()-1);
+}
 };
