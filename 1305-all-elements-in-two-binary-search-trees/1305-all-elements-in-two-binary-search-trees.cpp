8f0f48919e6349a0745de6db2da53e35a90a7147
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-01-26 10:21:36 +0530

    Time: 124 ms (93.52%), Space: 84.5 MB (79.25%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -1,23 +1,38 @@
-
-class Solution {
+class Solution
+{
 public:
-    vector<int>res;
-    void inorder(TreeNode*root){
-        if(root==NULL) return;
-        
-        
-        inorder(root->left);
-                res.push_back(root->val);
+     vector<int> getAllElements(TreeNode *root1, TreeNode *root2)
+     {
+          vector<int> res;
+          stack<TreeNode *> s1, s2;
 
-        inorder(root->right);
-        
-        return;
-            
-    }
-    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        inorder(root1);
-        inorder(root2);
-        sort(res.begin(),res.end());     //either merge the arrays or use stacks but don't sort as it O(m+n)log(m+n)
-        return res;
-    }
+          while (root1 || root2 || !s1.empty() || !s2.empty())
+          {
+               while (root1 != NULL)
+               {
+                    s1.push(root1);
+                    root1 = root1->left;
+               }
+               while (root2 != NULL)
+               {
+                    s2.push(root2);
+                    root2 = root2->left;
+               }
+               if (s2.empty() || (!s1.empty() && s1.top()->val <= s2.top()->val))
+               {
+                    root1 = s1.top();
+                    s1.pop();
+                    res.push_back(root1->val);
+                    root1 = root1->right;
+               }
+               else
+               {
+                    root2 = s2.top();
+                    s2.pop();
+                    res.push_back(root2->val);
+                    root2 = root2->right;
+               }
+          }
+          return res;
+     }
 };
