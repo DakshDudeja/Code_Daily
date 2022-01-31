a0cf347782ee2e1b1060416c2dab3fa92ea2567a
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-01-31 15:12:26 +0530

    Time: 12 ms (14.96%), Space: 7.9 MB (49.93%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int richest = 0;
+        for (auto &customer : accounts)
+            richest = max (richest, accumulate(customer.begin(), customer.end(), 0));
+        
+        return richest;
+    }
+};
