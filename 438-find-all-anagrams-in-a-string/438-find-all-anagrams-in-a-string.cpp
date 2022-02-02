985e5b03d989397757a384be443fb9524d72ad30
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-02-02 09:59:22 +0530

    Time: 22 ms (51.61%), Space: 8.8 MB (42.26%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -3,26 +3,24 @@ public:
     vector<int> findAnagrams(string s, string p) {
         vector<int>smp(26,0);
         vector<int>pmp(26,0);
-        vector<int>res;
-        if(s.size()<p.size()) return res;
+         vector<int>ans;               
+        if(s.size()<p.size()) return ans;
+        
         for(int i=0;i<p.size();i++){
-            pmp[p[i]-'a']++;
             smp[s[i]-'a']++;
+            pmp[p[i]-'a']++;
         }
-        int left=0,right=p.size()-1,len=s.size();
-        while(right < len){
+        int left=0,right=p.size()-1;
+        while(right<s.size()){
             
-            if(smp==pmp) 
-                res.push_back(left);
+            if(smp==pmp)
+                ans.push_back(left);                    
             right+=1;
-            if(right!=len)
-                smp[s[right]-'a']+=1;
-                smp[s[left]-'a']-=1;
-            
-            
+           if(right!=s.size())
+               smp[s[right]-'a']+=1;
+            smp[s[left]-'a']-=1;
             left+=1;
         }
-    return res;
-        
+        return ans;
     }
 };
