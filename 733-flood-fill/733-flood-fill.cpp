af05c1ca627f6a7f8db0eb8621b16cefb90f12d8
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-02-03 23:49:29 +0530

    Time: 11 ms (60.87%), Space: 14.3 MB (30.08%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& image, int sr, int sc, int newColor,int m,int n,int start){
-        if(sr>=m || sc>=n || sr<0 || sc<0 || image[sr][sc]!=start ) return;
-        
-        image[sr][sc] = newColor;
-        solve(image,sr+1,sc,newColor,m,n,start);
-        solve(image,sr,sc+1,newColor,m,n,start);
-        solve(image,sr-1,sc,newColor,m,n,start);
-        solve(image,sr,sc-1,newColor,m,n,start);
-        return;
-    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         if(image[sr][sc]==newColor) return image;
         int m=image.size();
         int n=image[0].size();
-        int start = image[sr][sc];
-        solve(image,sr,sc,newColor,m,n,start);
-        return image;
+        int color = image[sr][sc];
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        while(!q.empty()){
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+            
+            if(i<0 || j<0 || i>=m || j>=n || image[i][j]!=color) continue;
+            
+            image[i][j]=newColor;
+            q.push({i+1,j});
+            q.push({i,j+1});
+            q.push({i-1,j});
+            q.push({i,j-1});
+
+        }
+    return image;
     }
 };
