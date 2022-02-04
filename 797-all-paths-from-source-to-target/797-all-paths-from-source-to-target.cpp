851dfff0c40d51d8decdbb8809de57bf366fe2ae
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-02-04 12:42:14 +0530

    Time: 54 ms (16.66%), Space: 17.9 MB (15.95%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>>ans;
-        int n = graph.size();
-        vector<bool>visited(n);
-        vector<int>vec;
-        SourcetoTarget(graph,0,visited,n,vec,ans);
-        return ans;
-    }
-    void SourcetoTarget(vector<vector<int>>&graph,int currnode,vector<bool>&visited,int n, vector<int>currpath,vector<vector<int>>&ans)
-    {
-        if(currnode == n-1){
-            currpath.push_back(currnode);
-            ans.push_back(currpath);
-            currpath.pop_back();
-            return;
+        int n=graph.size();
+        vector<int>tmp;
+        tmp.push_back(0);
+        vector<vector<int>>res;
+        queue<vector<int>> q;
+        q.push(tmp);
+        while(!q.empty()){
+            tmp = q.front();
+            q.pop();
+            int currNode = tmp.back();
+            
+            if(currNode==n-1){
+                res.push_back(tmp);
+            } 
+            for(auto it:graph[currNode]){
+                vector<int>v(tmp);
+                v.push_back(it);
+                q.push(v);
+            }
         }
-        if(visited[currnode] == true) return;
-        
-        visited[currnode] = true;
-        currpath.push_back(currnode);
-        
-        for(auto neighbour: graph[currnode])
-            SourcetoTarget(graph,neighbour,visited,n,currpath,ans);
-        
-        visited[currnode] = false;
-        currpath.pop_back();
-        return;
+    
+        return res;
     }
 };
