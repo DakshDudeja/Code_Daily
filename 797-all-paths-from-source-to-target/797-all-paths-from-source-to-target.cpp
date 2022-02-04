527dd3036b076ac92c3cea4f2e85015d2df06f29
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-02-04 12:44:54 +0530

    Time: 24 ms (51.28%), Space: 16.7 MB (21.92%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int>tmp;
-        tmp.push_back(0);
-        vector<vector<int>>res;
+        vector<vector<int>> ret;
         queue<vector<int>> q;
-        q.push(tmp);
-        while(!q.empty()){
-            tmp = q.front();
+        int n = graph.size();
+        vector<int> temp;
+        temp.push_back(0);
+        q.push(temp);
+        while(!q.empty())
+        {
+            vector<int> vec = q.front();
+            int curr = vec[vec.size()-1];
+            if(curr == n-1)
+                ret.push_back(vec);
             q.pop();
-            int currNode = tmp.back();
-            
-            if(currNode==n-1){
-                res.push_back(tmp);
-            } 
-            for(auto it:graph[currNode]){
-                vector<int>v(tmp);
-                v.push_back(it);
-                q.push(v);
+            for(int i=0;i<graph[curr].size();i++)
+            {
+                
+                vec.push_back(graph[curr][i]);
+                q.push(vec);
+                vec.pop_back();
             }
         }
-    
-        return res;
+        return ret;
     }
 };
