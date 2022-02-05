1213077bae19157d73c885c3c4cf0b0f94994dff
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-02-05 10:31:32 +0530

    Time: 29 ms (19.58%), Space: 9.8 MB (32.15%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    bool solve(vector<vector<int>>& grid,int i,int j,int m,int n,vector<vector<bool>>&v){
+    bool solve(vector<vector<int>>& grid,int i,int j,int m,int n){
         if(i<0 || j<0||i>=m||j>=n) return false;
         if(grid[i][j]==1)return true;
-        if((i==0 || j==0 || i==m-1 || j==n-1) && grid[i][j]==0)
-            return false;
          
-        v[i][j]=true;
-        bool a=true;
+         grid[i][j]=1;
                 
-        if(!v[i+1][j])
-         a = a & solve(grid,i+1,j,m,n,v);
-        if(!v[i][j+1])
-         a = a& solve(grid,i,j+1,m,n,v);
-        if(!v[i-1][j])
-         a = a & solve(grid,i-1,j,m,n,v);
-        if(!v[i][j-1])
-         a =a & solve(grid,i,j-1,m,n,v);
-        return a;
+         bool a = solve(grid,i+1,j,m,n);
+         bool b=  solve(grid,i,j+1,m,n);
+         bool c =  solve(grid,i-1,j,m,n);
+         bool d = solve(grid,i,j-1,m,n);
+        
+        return a&b&c&d;
     }
     int closedIsland(vector<vector<int>>& grid) {
         int m=grid.size();
@@ -26,7 +20,7 @@ public:
         vector<vector<bool>>v(m,vector<bool>(n,false));
         for(int i=1;i<m-1;i++){
             for(int j=1;j<n-1;j++){
-                if(grid[i][j]==0 and v[i][j]==false and solve(grid,i,j,m,n,v)==true)
+                if(grid[i][j]==0  and solve(grid,i,j,m,n)==true)
                         cnt++;
             }
         }
