0f557645b5290522163a7a2196e897ede4ebbde0
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-02-05 16:55:50 +0530

    Time: 372 ms (60.04%), Space: 91.4 MB (50.55%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2,int i,int j,int m,int n){
-        if(i<0 || j<0 || i>=m || j>=n || grid2[i][j]==0)return true ;
+    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2,int i,int j,int m,int n,bool&flag){
+        if(i<0 || j<0 || i>=m || j>=n || grid2[i][j]==0)return  ;
         
-        if(grid1[i][j]==0) return false;
-        grid2[i][j]=0;
-         bool a =dfs(grid1,grid2,i+1,j,m,n);
-         bool b = dfs(grid1,grid2,i,j+1,m,n);
-         bool c= dfs(grid1,grid2,i-1,j,m,n);
-         bool  d= dfs(grid1,grid2,i,j-1,m,n);
+        if(grid1[i][j]==0) {
+            flag=false;
+        }
+         grid2[i][j]=0;
+         dfs(grid1,grid2,i+1,j,m,n,flag);
+         dfs(grid1,grid2,i,j+1,m,n,flag);
+         dfs(grid1,grid2,i-1,j,m,n,flag);
+         dfs(grid1,grid2,i,j-1,m,n,flag);
     
-        return a&b&c&d;
+        return;
     }
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         int m=grid2.size();
@@ -19,7 +21,9 @@ public:
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid2[i][j]==1 ){
-                    if(dfs(grid1,grid2,i,j,m,n)==true)
+                    bool flag=true;
+                    dfs(grid1,grid2,i,j,m,n,flag);
+                    if(flag)
                     cnt++;
                 }
             }
