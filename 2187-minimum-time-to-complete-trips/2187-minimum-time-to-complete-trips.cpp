2a033c47102af5420f8c890e25040c6954cd3a47
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-02-28 11:53:22 +0530

    Time: 812 ms (5.26%), Space: 88.9 MB (31.58%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -1,24 +1,32 @@
-#define pp 100000000000001
 class Solution {
 public:
-    long long minimumTime(vector<int>& time, int totalTrips) {
-        long long start=1;
-        long long end=pp;
-        long long ans=0;
-        while(start<=end){
-            long long mid=start+(end-start)/2;
-            long long trips=0;
-            for(auto e:time){
-                trips+=(mid/e);
-            }
-            if(trips<totalTrips){
-                start=mid+1;
-            }
-            else{
-                ans=mid;
-                end=mid-1;
-            }
+	// this function will count totalTrips for the given time
+	// a = [1,2,3] , and at time 3 how many trips we can take? 
+	// 3/1 + 3/2 + 3/3 => 3 + 1 + 1 = 5 Trips
+    long long int numberOfTripsForGivenTime(vector<int>&a , long long int givenTime)
+    {
+        long long int totalTrips = 0;
+        for(auto x : a)
+        {
+            // convert it to long long int 
+            long long int val = x;
+			
+            totalTrips += (givenTime / val);
         }
-        return ans;
+        return totalTrips;
+    }
+    long long minimumTime(vector<int>& arr ,  int totalTrips) {
+        long long int lowestTime = 1;
+        long long int highestTime = 1e14;
+        while(lowestTime<highestTime)
+        {
+            long long int mid = lowestTime + (highestTime-lowestTime)/2;
+            
+            if(numberOfTripsForGivenTime(arr , mid) >= totalTrips)
+                highestTime = mid;
+            else
+                lowestTime = mid+1;
+        }
+        return lowestTime;
     }
 };
