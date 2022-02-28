d98cc29604be9ac9cb634f9f9cde5c85bd1fa3d4
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-02-28 10:09:22 +0530

    Time: 16 ms (37.41%), Space: 17.3 MB (48.23%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -1,45 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(root == NULL)
-            return 0;
-        
-        int res = 1;
-        queue<pair<TreeNode*, int>> q;
-        
-        // I am using intialising list
-        q.push({root, 0});      // also can use make_pair
-        
-        while(!q.empty())
-        {
-            int cnt = q.size();
-            // start is the index of root node for first level
-            int start = q.front().second;
+        queue<pair<TreeNode*,int>>q;
+        q.push(make_pair(root,0));
+        int ans=0;
+        while(!q.empty()){
+            int sz=q.size();
+            int start =q.front().second;
             int end = q.back().second;
-            
-            res = max(res,end-start + 1);
-            
-            for(int i = 0; i <cnt; ++i)
-            {
-                pair<TreeNode*, int> p = q.front();
-                // we will use it while inserting it children
-                // left child will be 2 * idx + 1;
-                // right chils will be 2 * idx + 2;
-                int idx = p.second - start;
-                
+            ans=max(ans,end-start+1);
+            for(int i=0;i<sz;i++){
+                TreeNode* temp = q.front().first;
+                long long int cnt=q.front().second-start;
                 q.pop();
-                
-                // if  left child exist
-                if(p.first->left != NULL)
-                    q.push({p.first->left, (long long)2 * idx + 1});
-                
-                // if right child exist
-                if(p.first->right != NULL)
-                    q.push({p.first->right, (long long) 2 * idx + 2});
+                if(temp->left)q.push({temp->left,2*cnt+1});
+                if(temp->right)q.push({temp->right,2*cnt+2});
             }
+            
         }
-        
-        return res;
-        
+        return ans;
     }
 };
