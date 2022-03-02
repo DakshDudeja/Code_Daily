f482e1a9232f8880cee6d756fd09617b0fd282db
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-03-02 10:01:42 +0530

    Time: 3 ms (80.89%), Space: 5.9 MB (26.50%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -2,11 +2,18 @@ class Solution {
 public:
     int arrangeCoins(int n) {
         if(n==1) return 1;
-        long int i=1;
-        while(n>=0){
-               n=n-i;
-                i++;
+       long low=0,high=n,mid=0;
+        while(low<=high){
+             mid= low + (high-low)/2;
+            long target = (mid*(mid+1))/2;
+            if(target  == n)
+                return (int)mid;
+            if(target < n)
+                low=mid+1;
+            else
+                high=mid-1;
+                
         }
-        return i-2;        
+        return (int)high;
     }
 };
